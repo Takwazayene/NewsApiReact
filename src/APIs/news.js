90d47677ca7babfd50d66e8e62e43e67169f0f02
Takwazayene <takwa.zayene@esprit.tn>
2022-02-21 07:03:54 +0100

    before final

--- src/APIs/news.js
+++ src/APIs/news.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
 import { auth } from './auth';
+const config = require('../config.json');
+
 const endPointApiUrl = 'https://newsapi.org/v2/everything?q=actualités';
 const qInTitle = 'sport';
 const language = 'en';
-const apiKey = 'd04b4b00bc314bef8ed54f05c0ffc01b';
+const apiKey = config.api_key; 
 
 export function getNews(pageSize, page, searchKeyword) {
   return axios.get(`${endPointApiUrl}&qInTitle=${searchKeyword}&apiKey=${apiKey}`);
